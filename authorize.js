c6f92b3d78698ef3bf621fa6066951189bb66a25
masp1234 <mart2c04@stud.kea.dk>
2024-04-29 16:50:28 +0200

    throwing error on missing secret and claims added to request object

--- authorize.js
+++ authorize.js
@@ -3,21 +3,24 @@ import jwt from 'jsonwebtoken'
 function authorize(role, options = {}) {
     const {
         cookieName = '_auth',
-        secret = process.env.SECRET,
+        secret = process.env.AUTH_SECRET,
         rolePropertyName = 'userType',
     } = options;
+
+    if (!secret) {
+        throw new Error('No secret provided. Add AUTH_SECRET to .env or provide it in the options parameter.')
+    }
 
     return async (req, res, next) => {
         const token = req.cookies[cookieName];
         if (token) {
             const claims = jwt.verify(token, secret)
-            console.log(claims)
             if (claims[rolePropertyName] && claims[rolePropertyName] === role) {
-                console.log(claims[rolePropertyName])
-                return next()
+                req.user.claims = claims;
+                return next();
             }
         }
-        res.status(401).send({ message: `You are unauthorized. You must be role '${role}' or higher.`})
+        res.status(401).send({ message: `You are unauthorized. You must be role '${role}'.`})
     }
 }     
 
